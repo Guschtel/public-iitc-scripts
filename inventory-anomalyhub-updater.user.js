26749645265e68df45909a2826ddd99e5a3ee453
Guschtel <github@guschtel.de>
2025-06-09 11:11:15 +0200

    Refactor script for consistent formatting and indentation. Enhance logging, improve function structure, and adjust clipboard fallback logic.

--- inventory-anomalyhub-updater.user.js
+++ inventory-anomalyhub-updater.user.js
@@ -11,129 +11,130 @@
 // @grant        none
 // ==/UserScript==
 
-function to_number(val) {
-    if (val == null || val == undefined) {
-        return 0;
-    }
-    return +(val.toString().replace(/\r/g, ''));
-}
-
-function getOtherBeacons(inventory) {
-    var sum = 0;
-    for (const [key, value] of Object.entries(inventory)) {
-        if (key.startsWith("Powerup") && !key.startsWith("Powerup BB_BATTLE_RARE")) {
-            sum += +value;
-        }
-    }
-    return sum;
-}
-
-function setFormValue(formKey, formValue) {
-    const elements = document.getElementsByName(formKey);
-    if (elements != undefined && elements.length > 0) {
-        elements[0].value = formValue;
-    }
-}
-
-function isiOS() {
-    return (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream);
-}
-
-function isFirefox() {
-    return (/Firefox/i.test(navigator.userAgent));
-}
-
-function getClipboardContentFromDialog() {
-    return new Promise((resolve, reject) => {
-        console.log("Read clipboard from Dialog start");
-        const dialog = document.createElement('dialog');
-        const description = document.createElement('p');
-        const textarea = document.createElement('textarea');
-        const okButton = document.createElement('button');
-        const cancelButton = document.createElement('button');
-
-        console.log("Elements created");
-
-        dialog.style.padding = '1em';
-        description.textContent = 'The clipboard could not be accessed in your browser. Please paste your inventory into the textarea below.';
-        textarea.style.width = '400px';
-        textarea.style.height = '200px';
-        textarea.style.marginBottom = '1em';
-        okButton.textContent = 'OK';
-        cancelButton.textContent = 'Cancel';
-        okButton.style.marginRight = '1em';
-
-        console.log("Styles set");
-
-        dialog.appendChild(description);
-        dialog.appendChild(textarea);
-        dialog.appendChild(document.createElement('br'));
-        dialog.appendChild(okButton);
-        dialog.appendChild(cancelButton);
-
-        console.log("Dialog elements added");
-
-        okButton.onclick = () => {
-            dialog.close();
-            resolve(textarea.value);
-        };
-        cancelButton.onclick = () => {
-            dialog.close();
-            reject(new Error('Dialog for inserting clipboard content cancelled'));
-        };
-
-        document.body.appendChild(dialog);
-
-        console.log("Dialog added to body");
-
-        dialog.showModal();
-
-        console.log("Dialog shown");
-    });
-}
-
-function getClipboardContent() {
-    try {
-        if (isFirefox()) {
-            return navigator.clipboard.readText();
-        }
-        if (isiOS()) {
-            console.log("iOs: Read clipboard attempt");
-            if (!navigator.clipboard?.readText) {
-                console.log("iOs: Read clipboard content from dialog bc navigator.clipboard.readText is not available");
-                return getClipboardContentFromDialog();
-            }
-            console.log("iOs: Read clipboard normally");
-            return navigator.clipboard.readText();
-        } else {
-            // Chrome
-            return navigator.permissions.query({name: "clipboard-read"}).then(
-                (result) => {
-                    if (result.state === "granted" || result.state === "prompt") {
-                        return navigator.clipboard.readText();
-                    } else {
-                        return getClipboardContentFromDialog();
-                    }
-                },
-                (error) => {
-                    if (error.message.includes("clipboard-read")) {
-                        return getClipboardContentFromDialog();
-                    }
-                    throw error;
-                }
-            );
-        }
-    } catch (e) {
-        console.error('Failed to read clipboard contents in catch: ', e);
-        return getClipboardContentFromDialog();
-    }
-}
-
 (function() {
     'use strict';
 
     // Your code here...
     console.log('Running script! -------------------------------------');
+
+    function to_number(val) {
+        if (val == null || val == undefined) {
+            return 0;
+        }
+        return +(val.toString().replace(/\r/g, ''));
+    }
+
+    function getOtherBeacons(inventory) {
+        var sum = 0;
+        for (const [key, value] of Object.entries(inventory)) {
+            if (key.startsWith("Powerup") && !key.startsWith("Powerup BB_BATTLE_RARE")) {
+                sum += +value;
+            }
+        }
+        return sum;
+    }
+
+    function setFormValue(formKey, formValue) {
+        const elements = document.getElementsByName(formKey);
+        if (elements != undefined && elements.length > 0) {
+            elements[0].value = formValue;
+        }
+    }
+
+    function isiOS() {
+        return (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream);
+    }
+
+    function isFirefox() {
+        return (/Firefox/i.test(navigator.userAgent));
+    }
+
+    function getClipboardContentFromDialog() {
+        return new Promise((resolve, reject) => {
+            console.log("Read clipboard from Dialog start");
+            const dialog = document.createElement('dialog');
+            const description = document.createElement('p');
+            const textarea = document.createElement('textarea');
+            const okButton = document.createElement('button');
+            const cancelButton = document.createElement('button');
+
+            console.log("Elements created");
+
+            dialog.style.padding = '1em';
+            description.textContent = 'The clipboard could not be accessed in your browser. Please paste your inventory into the textarea below.';
+            textarea.style.width = '400px';
+            textarea.style.height = '200px';
+            textarea.style.marginBottom = '1em';
+            okButton.textContent = 'OK';
+            cancelButton.textContent = 'Cancel';
+            okButton.style.marginRight = '1em';
+
+            console.log("Styles set");
+
+            dialog.appendChild(description);
+            dialog.appendChild(textarea);
+            dialog.appendChild(document.createElement('br'));
+            dialog.appendChild(okButton);
+            dialog.appendChild(cancelButton);
+
+            console.log("Dialog elements added");
+
+            okButton.onclick = () => {
+                dialog.close();
+                resolve(textarea.value);
+            };
+            cancelButton.onclick = () => {
+                dialog.close();
+                reject(new Error('Dialog for inserting clipboard content cancelled'));
+            };
+
+            document.body.appendChild(dialog);
+
+            console.log("Dialog added to body");
+
+            dialog.showModal();
+
+            console.log("Dialog shown");
+        });
+    }
+
+    function getClipboardContent() {
+        try {
+            if (isFirefox()) {
+                return navigator.clipboard.readText();
+            }
+            if (isiOS()) {
+                console.log("iOs: Read clipboard attempt");
+                if (!navigator.clipboard?.readText) {
+                    console.log("iOs: Read clipboard content from dialog bc navigator.clipboard.readText is not available");
+                    return getClipboardContentFromDialog();
+                }
+                console.log("iOs: Read clipboard normally");
+                return navigator.clipboard.readText();
+            } else {
+                // Chrome
+                return navigator.permissions.query({name: "clipboard-read"}).then(
+                    async (result) => {
+                        if (result.state === "granted" || result.state === "prompt") {
+                            return await navigator.clipboard.readText();
+                        } else {
+                            return getClipboardContentFromDialog();
+                        }
+                    },
+                    (error) => {
+                        if (error.message.includes("clipboard-read")) {
+                            return getClipboardContentFromDialog();
+                        }
+                        throw error;
+                    }
+                );
+            }
+        } catch (e) {
+            console.error('Failed to read clipboard contents in catch: ', e);
+            return getClipboardContentFromDialog();
+        }
+    }
+
     var updateBtn = document.getElementsByClassName("btn btn-primary")[0];
 
     console.log('Update Button found');
@@ -149,6 +150,7 @@ function getClipboardContent() {
     console.log('Copy Button styles all set');
 
     copyCPbtn.onclick = function () {
+        console.log('Copy button clicked');
         getClipboardContent()
             .then(text => {
             console.log('Pasted content: ', text);
