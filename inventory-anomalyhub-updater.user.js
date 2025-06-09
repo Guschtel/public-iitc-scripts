dac6adb0418345281887a2237ebcc59754b1617e
Guschtel <github@guschtel.de>
2025-06-09 11:25:28 +0200

    Adjust iOs Detection

--- inventory-anomalyhub-updater.user.js
+++ inventory-anomalyhub-updater.user.js
@@ -18,14 +18,14 @@
     console.log('Running script! -------------------------------------');
 
     function to_number(val) {
-        if (val == null || val == undefined) {
+        if (val === null || val === undefined) {
             return 0;
         }
         return +(val.toString().replace(/\r/g, ''));
     }
 
     function getOtherBeacons(inventory) {
-        var sum = 0;
+        let sum = 0;
         for (const [key, value] of Object.entries(inventory)) {
             if (key.startsWith("Powerup") && !key.startsWith("Powerup BB_BATTLE_RARE")) {
                 sum += +value;
@@ -36,13 +36,13 @@
 
     function setFormValue(formKey, formValue) {
         const elements = document.getElementsByName(formKey);
-        if (elements != undefined && elements.length > 0) {
+        if (elements !== undefined && elements.length > 0) {
             elements[0].value = formValue;
         }
     }
 
     function isiOS() {
-        return (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream);
+        return (/iPad|iPhone|iPod|Macintosh|Mac OS X/.test(navigator.userAgent) && !window.MSStream);
     }
 
     function isFirefox() {
@@ -98,44 +98,44 @@
         });
     }
 
-    function getClipboardContent() {
+    async function getClipboardContentForIOs() {
+            console.log("iOs: Read clipboard start");
+            if (!navigator.clipboard?.readText) {
+                console.log("iOs: Read clipboard content from dialog bc navigator.clipboard.readText is not available");
+                return getClipboardContentFromDialog();
+            }
+            console.log("iOs: Read clipboard normally");
+            return await navigator.clipboard.readText();
+    }
+
+    function getClipboardContentForNonIOs() {
         try {
             if (isFirefox()) {
                 return navigator.clipboard.readText();
             }
-            if (isiOS()) {
-                console.log("iOs: Read clipboard attempt");
-                if (!navigator.clipboard?.readText) {
-                    console.log("iOs: Read clipboard content from dialog bc navigator.clipboard.readText is not available");
-                    return getClipboardContentFromDialog();
-                }
-                console.log("iOs: Read clipboard normally");
-                return navigator.clipboard.readText();
-            } else {
-                // Chrome
-                return navigator.permissions.query({name: "clipboard-read"}).then(
-                    async (result) => {
-                        if (result.state === "granted" || result.state === "prompt") {
-                            return await navigator.clipboard.readText();
-                        } else {
-                            return getClipboardContentFromDialog();
-                        }
-                    },
-                    (error) => {
-                        if (error.message.includes("clipboard-read")) {
-                            return getClipboardContentFromDialog();
-                        }
-                        throw error;
+            // Chrome
+            return navigator.permissions.query({name: "clipboard-read"}).then(
+                async (result) => {
+                    if (result.state === "granted" || result.state === "prompt") {
+                        return await navigator.clipboard.readText();
+                    } else {
+                        return getClipboardContentFromDialog();
                     }
-                );
-            }
+                },
+                (error) => {
+                    if (error.message.includes("clipboard-read")) {
+                        return getClipboardContentFromDialog();
+                    }
+                    throw error;
+                }
+            );
         } catch (e) {
             console.error('Failed to read clipboard contents in catch: ', e);
             return getClipboardContentFromDialog();
         }
     }
 
-    var updateBtn = document.getElementsByClassName("btn btn-primary")[0];
+    const updateBtn = document.getElementsByClassName("btn btn-primary")[0];
 
     console.log('Update Button found');
 
@@ -149,58 +149,80 @@
 
     console.log('Copy Button styles all set');
 
+    function parseClipboardContent(text) {
+        console.log('Pasted content: ', text);
+        if (text.includes('Type	Rarity	Count')) {
+            const lines = text.split("\n");
+            const inventory = {};
+            for (var line = 1; line < lines.length; line++) {
+                // By tabs
+                var tabs = lines[line].split("\t");
+                if (tabs[0] === "Shield" || tabs[0] === "HS" || tabs[0] === "Multi-Hack") {
+                    inventory[tabs[0] + " " + tabs[1]] = tabs[2];
+                } else {
+                    inventory[tabs[0]] = tabs[2];
+                }
+            }
+            console.log('Inventory: ', inventory);
+
+            setFormValue("inventory[X8]", to_number(inventory["XMP 8"]));
+            setFormValue("inventory[U8]", to_number(inventory["US 8"]));
+            setFormValue("inventory[JARVIS]", to_number(inventory["Virus JARVIS"]));
+            setFormValue("inventory[ADA]", to_number(inventory["Virus ADA"]));
+            setFormValue("inventory[R8]", to_number(inventory["Resonator 8"]));
+            setFormValue("inventory[R7]", to_number(inventory["Resonator 7"]));
+            setFormValue("inventory[R6]", to_number(inventory["Resonator 6"]));
+            setFormValue("inventory[R5]", to_number(inventory["Resonator 5"]));
+            setFormValue("inventory[R4]", to_number(inventory["Resonator 4"]));
+            setFormValue("inventory[Aegis]", to_number(inventory["Aegis Shield"]));
+            setFormValue("inventory[VR Shields]", to_number(inventory["Shield VERY_RARE"]));
+            setFormValue("inventory[C8]", to_number(inventory["PC 8"]));
+            setFormValue("inventory[Hypercubes]", to_number(inventory["Hypercube"]));
+            setFormValue("inventory[LPC]", to_number(inventory["Hypercube"]));
+            setFormValue("inventory[VR Battle Beacons]", to_number(inventory["Powerup BB_BATTLE"]));
+            setFormValue("inventory[Other Beacons]", to_number(getOtherBeacons(inventory)));
+            setFormValue("inventory[VRHS]", to_number(inventory["HS VERY_RARE"]));
+            setFormValue("inventory[VRMH]", to_number(inventory["Multi-Hack VERY_RARE"]));
+            setFormValue("inventory[SBUL]", to_number(inventory["Ultra-Link"]));
+            setFormValue("inventory[FRK]", to_number(inventory["Powerup FRACK"]));
+            setFormValue("inventory[Caps]", to_number(inventory["Capsule"]));
+            const utc = new Date().toJSON();
+            const commentElement = document.getElementsByName("inventory[_notes]")[0];
+            if (!commentElement.value.includes("Inventory automatically filled with Guschtels Inventory userscripts")) {
+                commentElement.value += "\nInventory automatically filled with Guschtels Inventory userscripts (https://github.com/Guschtel/public-iitc-scripts/tree/main) on " + utc;
+            } else {
+                const regex = /on [0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.*Z/i;
+                commentElement.value = commentElement.value.replace(regex, 'on ' + utc);
+            }
+        } else {
+            console.error('Invalid clipboard content');
+        }
+    }
+
     copyCPbtn.onclick = function () {
         console.log('Copy button clicked');
-        getClipboardContent()
-            .then(text => {
-            console.log('Pasted content: ', text);
-            if (text.includes('Type	Rarity	Count')) {
-                var lines = text.split("\n");
-                var inventory = {};
-                for(var line = 1; line < lines.length; line++){
-                    // By tabs
-                    var tabs = lines[line].split("\t");
-                    if (tabs[0] == "Shield" || tabs[0] == "HS" || tabs[0] == "Multi-Hack") {
-                        inventory[tabs[0] + " " + tabs[1]] = tabs[2];
-                    } else {
-                        inventory[tabs[0]] = tabs[2];
-                    }
-                }
-                console.log('Inventory: ', inventory);
-
-                setFormValue("inventory[X8]", to_number(inventory["XMP 8"]));
-                setFormValue("inventory[U8]", to_number(inventory["US 8"]));
-                setFormValue("inventory[JARVIS]", to_number(inventory["Virus JARVIS"]));
-                setFormValue("inventory[ADA]", to_number(inventory["Virus ADA"]));
-                setFormValue("inventory[R8]", to_number(inventory["Resonator 8"]));
-                setFormValue("inventory[R7]", to_number(inventory["Resonator 7"]));
-                setFormValue("inventory[R6]", to_number(inventory["Resonator 6"]));
-                setFormValue("inventory[R5]", to_number(inventory["Resonator 5"]));
-                setFormValue("inventory[R4]", to_number(inventory["Resonator 4"]));
-                setFormValue("inventory[Aegis]", to_number(inventory["Aegis Shield"]));
-                setFormValue("inventory[VR Shields]", to_number(inventory["Shield VERY_RARE"]));
-                setFormValue("inventory[C8]", to_number(inventory["PC 8"]));
-                setFormValue("inventory[Hypercubes]", to_number(inventory["Hypercube"]));
-                setFormValue("inventory[LPC]", to_number(inventory["Hypercube"]));
-                setFormValue("inventory[VR Battle Beacons]", to_number(inventory["Powerup BB_BATTLE"]));
-                setFormValue("inventory[Other Beacons]", to_number(getOtherBeacons(inventory)));
-                setFormValue("inventory[VRHS]", to_number(inventory["HS VERY_RARE"]));
-                setFormValue("inventory[VRMH]", to_number(inventory["Multi-Hack VERY_RARE"]));
-                setFormValue("inventory[SBUL]", to_number(inventory["Ultra-Link"]));
-                setFormValue("inventory[FRK]", to_number(inventory["Powerup FRACK"]));
-                setFormValue("inventory[Caps]", to_number(inventory["Capsule"]));
-                var utc = new Date().toJSON();
-                var commentElement = document.getElementsByName("inventory[_notes]")[0];
-                if (!commentElement.value.includes("Inventory automatically filled with Guschtels Inventory userscripts")) {
-                    commentElement.value += "\nInventory automatically filled with Guschtels Inventory userscripts (https://github.com/Guschtel/public-iitc-scripts/tree/main) on " + utc;
-                } else {
-                    const regex = /on [0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.*Z/i;
-                    commentElement.value = commentElement.value.replace(regex, 'on ' + utc);
-                }
-            } else {
-                console.error('Invalid clipboard content');
+        if (isiOS()) {
+            console.log('iOs detected');
+            try {
+                getClipboardContentForIOs().then(text => {
+                    console.log('Read clipboard content for iOs: ', text);
+                    parseClipboardContent(text);
+                })
+            } catch (e) {
+                console.error('Failed to read iOs clipboard contents: ', e);
+                getClipboardContentFromDialog().then(text => {
+                    console.error('Failed to read iOs clipboard contents: ', e);
+                    parseClipboardContent(text)
+                })
             }
-        })
+            return;
+        }
+        console.log('Proceeding with non-iOs');
+        getClipboardContentForNonIOs()
+            .then(text => {
+                console.log('Read clipboard content for non-iOs Browsers: ', text);
+                parseClipboardContent(text);
+            })
             .catch(err => {
             console.error('Failed to read clipboard contents: ', err);
         });
